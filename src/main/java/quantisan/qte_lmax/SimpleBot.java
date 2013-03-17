740bea674f9d7958b29a436e98292b97fc69c9c0
Paul Lam <paul@quantisan.com>
2013-03-17 10:37:16 +0000

    can subscribe to gbpusd

--- src/main/java/quantisan/qte_lmax/SimpleBot.java
+++ src/main/java/quantisan/qte_lmax/SimpleBot.java
@@ -1,13 +1,18 @@
 package quantisan.qte_lmax;
 
+import com.lmax.api.Callback;
 import com.lmax.api.FailureResponse;
 import com.lmax.api.LmaxApi;
 import com.lmax.api.Session;
 import com.lmax.api.account.LoginCallback;
 import com.lmax.api.account.LoginRequest;
+import com.lmax.api.orderbook.OrderBookEvent;
+import com.lmax.api.orderbook.OrderBookEventListener;
+import com.lmax.api.orderbook.OrderBookSubscriptionRequest;
 
-public class SimpleBot implements LoginCallback {
+public class SimpleBot implements LoginCallback, OrderBookEventListener {
 
+    private final static long GBP_USD_INSTRUMENT_ID = 4001;
     private Session session;
 
     public static void main(String[] args) {
@@ -22,11 +27,31 @@ public class SimpleBot implements LoginCallback {
     public void onLoginSuccess(Session session) {
         System.out.printf("Logged in, account details: %s%n", session.getAccountDetails());
 
-        this.session = session; // Capture the session for use later.
+        this.session = session;
+        session.registerOrderBookEventListener(this);
+        session.subscribe(new OrderBookSubscriptionRequest(GBP_USD_INSTRUMENT_ID), new Callback()
+        {
+            public void onSuccess()
+            {
+                System.out.println("Successful subscription");
+            }
+
+            public void onFailure(FailureResponse failureResponse)
+            {
+                System.err.printf("Failed to subscribe: %s%n", failureResponse);
+            }
+        });
+
+        session.start();
     }
 
     @Override
     public void onLoginFailure(FailureResponse failureResponse) {
         System.err.printf("Failed to login, reason: %s%n", failureResponse);
+    }
+
+    @Override
+    public void notify(OrderBookEvent orderBookEvent) {
+        System.out.printf("Market data: %s%n", orderBookEvent);
     }
 }
