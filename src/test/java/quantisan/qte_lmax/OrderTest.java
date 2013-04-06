4dc04028f04cb704a214d5535eaf29b9d11f0fe3
Paul Lam <paul@quantisan.com>
2013-04-06 14:33:27 +0100

    order test parsing edn message to order spec

--- src/test/java/quantisan/qte_lmax/OrderTest.java
+++ src/test/java/quantisan/qte_lmax/OrderTest.java
@@ -1,10 +1,22 @@
 package quantisan.qte_lmax;
 
+import com.lmax.api.FixedPointNumber;
+import com.lmax.api.TimeInForce;
+import com.lmax.api.order.MarketOrderSpecification;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class OrderTest {
     @Test
-    public void testExecute() throws Exception {
+    public void testToMarketOrder() throws Exception {
+        long instrument = 4001;
+        String orderId = "my order 1";
+        FixedPointNumber quantity = FixedPointNumber.valueOf(1);
+        FixedPointNumber stopLossOffset = FixedPointNumber.valueOf(10);
+        MarketOrderSpecification mos = new MarketOrderSpecification(instrument, orderId, quantity, TimeInForce.IMMEDIATE_OR_CANCEL,stopLossOffset, null);
 
+        assertEquals(mos.toString(),
+                Order.toMarketOrder("{:instrument \"EURUSD\", :order-id \"my order 1\", :quantity 1, :stop-loss-offset 10}").toString());
     }
 }
